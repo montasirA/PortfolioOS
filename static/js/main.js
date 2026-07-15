4ffeb643812a242e4d0f7ec52d3245f27bba5ae6
montasirA <arafinmontasir8@gmail.com>
2026-07-15 18:56:27 +0600

    Improve hero section and mobile responsiveness

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,30 @@
-console.log("Portfolio OS Loaded Successfully!");
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+if(menuBtn){
+
+    menuBtn.addEventListener("click", ()=>{
+
+        navLinks.classList.toggle("active");
+
+        menuBtn.innerHTML = navLinks.classList.contains("active")
+            ? "✕"
+            : "☰";
+
+    });
+
+}
+
+document.querySelectorAll(".nav-links a").forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        navLinks.classList.remove("active");
+
+        if(menuBtn){
+            menuBtn.innerHTML="☰";
+        }
+
+    });
+
+});
